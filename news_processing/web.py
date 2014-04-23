3f5925e384a37031f83f7d294a5f5bd8b4aaec88
duc07 <ducnguyen1911@gmail.com>
2014-04-23 05:14:08 -0500

    Complete: displaying Topic & tweets in Google map

--- news_processing/web.py
+++ news_processing/web.py
@@ -1,23 +1,20 @@
-from crawler import settings
-from news_processing import news_cluster
-
 __author__ = 'duc07'
 
-
-import os
 import cgi
-import sys
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import cPickle as pickle
+
 
 class customHTTPServer(BaseHTTPRequestHandler):
         def do_GET(self):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                self.wfile.write('<HTML><body>')
-                self.write_content()
-                self.wfile.write('</body></HTML>')
+                self.wfile.write('<HTML>\n')
+                self.write_head()
+                self.wfile.write('<body>\n')
+                self.write_body()
+                self.wfile.write('</body></HTML>\n')
                 return
 
         def do_POST(self):
@@ -29,18 +26,71 @@ class customHTTPServer(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write('Post!')
 
-        def write_content(self):
-                print 'write content'
-                ls_topics = pickle.load( open( "topics.p", "rb" ) )
+        def write_head(self):
+                self.wfile.write('<head>\n')
+                self.wfile.write('  <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />\n')
+                self.wfile.write('  <style type="text/css">\n')
+                self.wfile.write('      html { height: 100% }\n')
+                self.wfile.write('      body { height: 100%; margin: 0; padding: 0 }\n')
+                self.wfile.write('      #map-canvas { height: 100% }\n')
 
-                print '1'
+                self.wfile.write('      .labels {\n')
+                self.wfile.write('          color: red;\n')
+                self.wfile.write('          background-color: white;\n')
+                self.wfile.write('          font-family: "Lucida Grande", "Arial", sans-serif;\n')
+                self.wfile.write('          font-size: 10px;\n')
+                self.wfile.write('          font-weight: bold;\n')
+                self.wfile.write('          text-align: center;\n')
+                # self.wfile.write('          width: 40px;\n')
+                self.wfile.write('          border: 2px solid black;\n')
+                self.wfile.write('          white-space: nowrap;\n')
+                self.wfile.write('      }\n')
+
+                self.wfile.write('  </style>\n')
+                self.wfile.write('  <script type="text/javascript"\n')
+                self.wfile.write('      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7Rv-d2-b5gzM8NvJJQMsIeXp7ByIHJlw&sensor=true">\n')
+                self.wfile.write('  </script>\n')
+                self.wfile.write('<script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/tags/markerwithlabel/1.1.9/src/markerwithlabel.js"></script>')
+                self.wfile.write('  <script type="text/javascript">\n')
+                self.wfile.write('      function initialize() {\n')
+                self.wfile.write('          var mapOptions = {\n')
+                self.wfile.write('              center: new google.maps.LatLng(39.50, -98.35),\n')
+                self.wfile.write('              zoom: 5\n')
+                self.wfile.write('          };\n')
+                self.wfile.write('          var map = new google.maps.Map(document.getElementById("map-canvas"),\n')
+                self.wfile.write('              mapOptions);\n')
+
+                ls_topics = pickle.load(open("topics.p", "rb"))
+                i = 0
                 for topic in ls_topics:
-                    self.wfile.write('<p>Topic: ' + topic[0] + '</p>')
-                    self.wfile.write('<p>Topic Geo Center: ' + str(topic[2][0]) + ' : ' + str(topic[2][1]) + '</p>')
-                    self.wfile.write('<p>Topic Score: ' + str(topic[3]) + '--</p>')
-                    for tweet in topic[1]:
-                        self.wfile.write('<p> Tweet: ' + self.encode(tweet['text']) + '</p>')
-                    self.wfile.write('<p>------------------------------------</p>')
+                    i += 1
+
+                    self.wfile.write('var marker'+str(i)+' = new MarkerWithLabel({\n')
+                    self.wfile.write('position: new google.maps.LatLng('+str(topic[2][1])+','+str(topic[2][0])+'),\n')
+                    self.wfile.write('draggable: true,\n')
+                    self.wfile.write('raiseOnDrag: true,\n')
+                    self.wfile.write('map: map,\n')
+                    self.wfile.write('labelContent: "'+self.compact(topic[0])+'",\n')
+                    self.wfile.write('labelAnchor: new google.maps.Point(22, 0),\n')
+                    self.wfile.write('labelClass: "labels", // the CSS class for the label\n')
+                    self.wfile.write('labelStyle: {opacity: 0.75}\n')
+                    self.wfile.write('});\n')
+                    self.wfile.write('var iw'+str(i)+' = new google.maps.InfoWindow({\n')
+                    self.wfile.write('content: "'+self.get_tweets(topic)+'"\n')
+                    self.wfile.write('});\n')
+                    self.wfile.write('google.maps.event.addListener(marker'+str(i)+', "click", function (e) { iw'+str(i)+'.open(map, this); });\n')
+
+                    if i > 15:
+                        break
+
+                self.wfile.write('          }\n')
+                self.wfile.write('      google.maps.event.addDomListener(window, "load", initialize);\n')
+                self.wfile.write('  </script>\n')
+                self.wfile.write('</head>\n')
+
+        def write_body(self):
+                print 'write body'
+                self.wfile.write('<div id="map-canvas"/>\n')
 
         def encode(self, text):
             """
@@ -48,9 +98,23 @@ class customHTTPServer(BaseHTTPRequestHandler):
             """
             return text.encode('utf-8')
 
+        def compact(self, label_topic):
+            label = ''
+            words = label_topic.split('+')
+            for w in words:
+                ws = w.split('*')
+                label += ws[1] + ','
+            return label
+
+        def get_tweets(self, topic):
+            result = ''
+            for tweet in topic[1]:
+                result += '<p>' + self.encode(tweet['text']) + '</p>'
+            return result.replace('\n', '').replace('#', '').replace('@', '').replace('"', '')
+
 def main():
         try:
-                server = HTTPServer(('',8080),customHTTPServer)
+                server = HTTPServer(('', 8080), customHTTPServer)
                 print 'server started at port 8080'
                 server.serve_forever()
         except KeyboardInterrupt:
